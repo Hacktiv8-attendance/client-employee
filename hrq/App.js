134121ca08f376c7da37b31983e6323e4a23e8d6
xavierThufail <xavier.thufail@gmail.com>
2020-04-18 01:38:24 +0700

    create login screen

--- hrq/App.js
+++ hrq/App.js
@@ -1,67 +1,47 @@
-import * as React from 'react';
+import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { SplashScreen } from 'expo';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { compose, applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import BottomTabNavigator from './navigation/BottomTabNavigator';
-import useLinking from './navigation/useLinking';
+import LoginScreen from './screens/LandingScreen';
+import rootReducers from './store/reducers/rootReducers';
 
 const Stack = createStackNavigator();
+const store = createStore(rootReducers, compose(
+  applyMiddleware(thunk)
+))
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
-  const [initialNavigationState, setInitialNavigationState] = React.useState();
-  const containerRef = React.useRef();
-  const { getInitialState } = useLinking(containerRef);
-
-  // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHide();
-
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
-
-        // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font,
-          'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hide();
-      }
-    }
-
-    loadResourcesAndDataAsync();
-  }, []);
-
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null;
-  } else {
-    return (
+export default function App() {
+  return (
+    <Provider store={store}>
       <View style={styles.container}>
         {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
+        <NavigationContainer>
           <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+            name="Root"
+            component={BottomTabNavigator} />
           </Stack.Navigator>
         </NavigationContainer>
       </View>
-    );
-  }
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e4f9f5',
   },
 });
---
+++ hrq/screens/LandingScreen.js
@@ -0,0 +1,202 @@
+import React, { useState, useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { ScrollView } from 'react-native-gesture-handler';
+import { StyleSheet,View, Text, Image, TextInput, TouchableOpacity, AsyncStorage } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Constant from 'expo-constants';
+import allAction from '../store/actions';
+
+
+export default function LandingScreen () {
+  let user = useSelector(state => state.user)
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const linkForgotPassword = (
+    <Text style={styles.textHelpLink}>Click here.</Text>
+  )
+
+  const retrieveData = async () => {
+    try {
+      let value = await AsyncStorage.getItem('userStorage')
+      value = JSON.parse(value)
+      if (value) {
+        console.log('dapet data: ====== ', value)
+        dispatch(allAction.user.setUser(value))
+        navigation.navigate('Root')
+      } else {
+        console.log('kosooooong')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect (() => {
+    retrieveData()
+  }, [])
+
+  const storeData = async (user) => {
+    try {
+      console.log(user, '==========store data================')
+      await AsyncStorage.setItem('userStorage', JSON.stringify(user))
+      navigation.navigate('Root')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleLogin = () => {
+    console.log('masuk')
+    dispatch(allAction.user.login({email, password}))
+    storeData({ token: user.token, payload: user.payload })
+    setEmail('')
+    setPassword('')
+  }
+
+
+  function handleEmail (email) {
+    setEmail(email)
+  }
+
+  function handlePassword (password) {
+    setPassword(password)
+  }
+
+  if(user.loading) return (
+    <View>
+      <Text>Loading</Text>
+    </View>
+  )
+
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <View>
+          <Text style={styles.textHeader}>Welcome to HRQ</Text>
+        </View>
+      
+        <View style={styles.logoContainer}>
+          <Image
+            source={require('../assets/images/login-logo.jpeg')}
+            style={styles.logoImage}
+          />
+        </View>
+
+        <View>
+          <Text style={styles.textLabel}>Email: </Text>
+
+          <TextInput
+            value={email}
+            onChangeText={email => handleEmail(email)}
+            placeholder='example@example.com'
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            style={styles.input}
+          />
+
+          <Text style={styles.textLabel}>Password: </Text>
+
+          <TextInput
+            value={password}
+            onChangeText={password => handlePassword(password)}
+            placeholder='password'
+            textContentType="password"
+            secureTextEntry={true}
+            style={styles.input}
+          />
+
+        </View>
+        
+        <View>
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.containerHelp}>
+          <Text style={styles.textHelp}>forgot Password? {linkForgotPassword}</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e4f9f5',
+  },
+  contentContainer: {
+    paddingTop: Constant.statusBarHeight,
+  },
+  containerText: {
+    marginLeft: 30,
+    marginBottom: 10
+  },
+  containerHelp: {
+    marginTop: 10
+  },
+  textLogin: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  textHeader: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginTop: 30,
+  },
+  buttonText: {
+    textAlign: 'center',
+  },
+  textLabel: {
+    marginLeft: 30,
+    marginBottom: 10,
+  },
+  textHelp: {
+    marginLeft: 30,
+  },
+  textHelpLink: {
+    color: '#30e3ca'
+  },
+  logoImage: {
+    width: 200,
+    height: 160,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    paddingLeft: 10,
+    marginRight: 30,
+    marginLeft: 30,
+    marginBottom: 30,
+    maxWidth: 700,
+    height: 50,
+    borderColor: '#30e3ca',
+    borderRadius: 5,
+  },
+  button: {
+    display: 'flex',
+    height: 50,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#11999e',
+    shadowColor: '#11999e',
+    shadowOpacity: 0.4,
+    shadowOffset: { height: 10, width: 0 },
+    shadowRadius: 20,
+    marginLeft: 30,
+    marginRight: 30
+  }
+});
